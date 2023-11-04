0c78299f744c617a31000c6e4eb7eff8efcd614f
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-04 16:19:34 -0300

    :construction: add src folder

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.listen(() => {
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(PORT, () => {
   console.log(`[server] Server is running at: http://localhost:${PORT}`);
 });
