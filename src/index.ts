ebebdc0f47f4b7266ecdb767d4463b357cb17148
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-05 21:06:04 -0300

    :construction: add server configs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { router as authRouter } from "./routes/auth-router";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3030;
+
+app.use(cors());
+app.use(express.json());
+app.use(authRouter);
 
 app.get("/", (req, res) => {
   res.send("hello world!");
