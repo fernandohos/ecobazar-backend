9e9c477ac01cd1c6eec77fd363effd8182537b37
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-09 20:29:48 -0300

    :sparkles: add user resgiter and login model functions

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,17 +1,39 @@
+import { db } from "../database";
+
 type User = {
-  id?: string;
   name: string;
   email: string;
   password: string;
-}
+};
 
 async function createUser(user: User) {
-  console.log(user)
+  const client = await db.connect();
+  try {
+    const query =
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)";
+    const values = Object.values(user);
+    await client.query(query, values);
+  } catch (error) {
+    console.error("Error creating user: ", error);
+  } finally {
+    client.release();
+  }
 }
 
-async function getUserByEmail(email: string): Promise<User | null> {
-  console.log(email);
-  return null;
+async function getUserByEmail(
+  email: string
+): Promise<(User & { id: string }) | undefined> {
+  const client = await db.connect();
+  try {
+    const query = "SELECT * FROM users WHERE users.email = $1";
+    const { rows } = await client.query<any>(query, [email]);
+
+    return rows[0];
+  } catch (error) {
+    console.error("Error getting user: ", error);
+  } finally {
+    client.release();
+  }
 }
 
-export default {createUser, getUserByEmail};
+export default { createUser, getUserByEmail };
