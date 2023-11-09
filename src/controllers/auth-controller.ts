291467b497e84b513db8db39e78dd6a758207ffa
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-09 20:28:52 -0300

    :recycle: refactor auth controller

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -3,7 +3,10 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { validateEmptyFields } from "../helpers/validate-empty-fields";
 import { validateEmail } from "../helpers/validate-email";
-import userModel from '../models/user-model';
+import { capitalizeFirstLetters } from "../helpers/capitalize-first-letters";
+import userModel from "../models/user-model";
+import dotenv from "dotenv";
+dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 const saltRounds = 10; // bcrypt salt rounds
@@ -12,7 +15,7 @@ async function register(req: Request, res: Response) {
   const { name, email, password } = req.body;
 
   // validate fields
-  if (validateEmptyFields({ name, email, password })) {
+  if (!validateEmptyFields({ name, email, password })) {
     res.status(400).json({ message: "All inputs is required!" });
     return;
   }
@@ -35,33 +38,42 @@ async function register(req: Request, res: Response) {
   }
 
   // validate if user exists
-  const existingUser = await userModel.getUserByEmail(email.toLowerCase());
+  try {
+    const existingUser = await userModel.getUserByEmail(email.toLowerCase());
 
-  if (existingUser) {
-    res.status(409).json({ message: "User already exists!" });
-    return;
+    if (existingUser) {
+      res.status(409).json({ message: "User already exists!" });
+      return;
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error." });
+    console.error("Error validating if user exists: ", error);
   }
 
   // hash password before save
   const hashedPassword = await bcrypt.hash(password, saltRounds);
 
   const user = {
-    name,
-    email,
+    name: capitalizeFirstLetters(name),
+    email: email.toLowerCase(),
     password: hashedPassword,
   };
 
   // create user
-  const createdUser = await userModel.createUser(user);
+  try {
+    const createdUser = await userModel.createUser(user);
 
-  // create JWT token
-  const token = jwt.sign(user, JWT_SECRET, {
-    expiresIn: "1h",
-  });
+    // create JWT token
+    const token = jwt.sign(user, `${JWT_SECRET}`, {
+      expiresIn: "1h",
+    });
 
-  return res
-    .status(201)
-    .json({ message: "User Created Successfully!", token });
+    res.status(201).json({ message: "User Created Successfully!", token });
+    return;
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error." });
+    console.error("Error creating user: ", error);
+  }
 }
 
 async function login(req: Request, res: Response) {
@@ -86,25 +98,32 @@ async function login(req: Request, res: Response) {
   }
 
   // validate if user exists
-  const user = await userModel.getUserByEmail(email);
+  try {
+    const user = await userModel.getUserByEmail(email);
 
-  if (!user) {
-    return res.status(401).json({ message: "Invalid credentials!" });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
+
+    // compare password with hashed password saved on db
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
+
+    // create JWT token
+    const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return res
+      .status(200)
+      .json({ message: "User Logged Successfully!", token });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error." });
+    console.error(error);
   }
-
-  // compare password with hashed password saved on db
-  const passwordMatch = await bcrypt.compare(password, user.password);
-
-  if (!passwordMatch) {
-    return res.status(401).json({ message: "Invalid credentials!" });
-  }
-
-  // create JWT token
-  const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
-    expiresIn: "1h",
-  });
-
-  return res.status(200).json({ message: "User Logged Successfully!", token });
 }
 
 export default { login, register };
