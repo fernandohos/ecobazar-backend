9e3abf2b53b479366871c484721185b6d98b350b
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-11 12:14:04 -0300

    :card_file_box: add password, created_at and updated_at to table users

--- src/database/create-table-users.sql
+++ src/database/create-table-users.sql
@@ -2,11 +2,14 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
     street_address VARCHAR(100),
     country VARCHAR(50),
     state VARCHAR(50),
     zip_code VARCHAR(20),
-    email VARCHAR(100) UNIQUE NOT NULL,
     phone VARCHAR(20),
-    avatar_url VARCHAR(200)
+    avatar_url VARCHAR(200),
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL
 );
