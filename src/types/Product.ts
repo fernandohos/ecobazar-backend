d7ee9f7f437f301401b38d5dd6a94529e6228b11
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-11 19:23:15 -0300

    :label: add product type

---
+++ src/types/Product.ts
@@ -0,0 +1,17 @@
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  sku?: string | null;
+  price: number;
+  old_price?: number | null;
+  category: string;
+  tags: string[];
+  stock: number;
+  weight: number;
+  color: string;
+  type: string;
+  unit_type: string;
+  created_at: number;
+  updated_at: number;
+};
