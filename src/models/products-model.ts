a511596302728c4d549fef43af36eb1fe696f837
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-11 23:25:20 -0300

    :construction: add products model

---
+++ src/models/products-model.ts
@@ -0,0 +1,70 @@
+import { QueryResult } from "pg";
+import { db } from "../database";
+import { Product } from "../types/Product";
+
+export type FiltersType = {
+  category: string;
+  min_price: number;
+  max_price: number;
+  tags: string[];
+  page_size: number;
+  page_offset: number;
+};
+
+async function getProducts(filters: FiltersType) {
+  console.log("got filters", filters);
+  const client = await db.connect();
+  const { category, min_price, max_price, tags, page_size, page_offset } =
+    filters;
+  const values = [category, min_price, max_price, tags, page_size, page_offset];
+
+  function QueryProducts(): Promise<QueryResult<Product>> {
+    const productsQuery = `
+    SELECT *
+    FROM products
+    WHERE
+      (category = $1 OR $1 IS NULL)
+      AND (price >= $2 OR $2 IS NULL)
+      AND (price <= $3 OR $3 IS NULL)
+      AND (tags @> ARRAY[$4]::text[] OR $4 IS NULL)
+      LIMIT $5
+      OFFSET $6
+  `;
+    return new Promise((resolve) => {
+      resolve(client.query(productsQuery, values));
+    });
+  }
+
+  function QueryCount(): Promise<QueryResult<{ count: number }>> {
+    const countQuery = `
+      SELECT COUNT(*)
+      FROM products
+      WHERE
+        (category = $1 OR $1 IS NULL)
+        AND (price >= $2 OR $2 IS NULL)
+        AND (price <= $3 OR $3 IS NULL)
+        AND (tags @> ARRAY[$4]::text[] OR $4 IS NULL)
+  `;
+    return new Promise((resolve) => {
+      resolve(client.query(countQuery, values.slice(0, -2)));
+    });
+  }
+
+  try {
+    return await Promise.all([QueryProducts(), QueryCount()])
+      .then((r) => {
+        console.log("DATABASE RESPONSE", r);
+        return r;
+      })
+      .then((res) => ({
+        products: res[0].rows,
+        ...res[1].rows[0],
+      }));
+  } catch (error) {
+    console.error("Error querying products: ", error);
+  } finally {
+    client.release();
+  }
+}
+
+export default { getProducts };
